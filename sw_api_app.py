70ffd685a7401f142c0e00e31161ce1f667cc36c
Vishal Tripathi <106934872+vish-bear@users.noreply.github.com>
2022-06-08 02:58:09 -0500

    Bug Test regarding headers

--- sw_api_app.py
+++ sw_api_app.py
@@ -6,7 +6,7 @@ from fake_useragent import UserAgent
 
 
 ua = UserAgent()
-AGENT = {'User-Agent':str(ua.chrome)}#,"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+AGENT = {'User-Agent':str(ua.chrome),"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
 
 
 ### Sidebar work below ###
@@ -64,7 +64,10 @@ domain = st.text_input('Please enter the domain name of the company you want to 
 
 resp = SW_API.swGet(domain, headers = AGENT)
 
+
+
 if resp.status_code != 200:
+    resp.raise_for_status()
     st.error('Error Code : %s'%resp.status_code)
 
 if resp.status_code == 200:
