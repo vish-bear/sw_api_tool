42620115cb87bb0549bbb01a593a768ac7cc710f
Vishal Tripathi <106934872+vish-bear@users.noreply.github.com>
2022-06-08 02:51:58 -0500

    Updating Proc File
    Updating Proc File

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run --server.port $PORT sw_api_app.py
--- sw_api_app.py
+++ sw_api_app.py
@@ -6,7 +6,7 @@ from fake_useragent import UserAgent
 
 
 ua = UserAgent()
-AGENT = {'User-Agent':str(ua.chrome),"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+AGENT = {'User-Agent':str(ua.chrome)}#,"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
 
 
 ### Sidebar work below ###
