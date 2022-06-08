c8a5326fabe7eedb43f28061d02b9d32a8f2b950
Vishal Tripathi <106934872+vish-bear@users.noreply.github.com>
2022-06-08 03:08:24 -0500

    testing a bug

--- SW_API.py
+++ SW_API.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 import pandas as pd
 import ast
     
-AGENT = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+#AGENT = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "DNT": "1", "Connection": "close", "Upgrade-Insecure-Requests": "1"}
 
 COUNTRY_CODES = pd.read_csv('wikipedia-iso-country-codes.csv')
 
